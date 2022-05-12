fe2a60706f944c05f4d5f58b6d8ee17b57c1a44f
Alvaro Prior <alvaro.prior@student.ie.edu>
2022-05-12 02:08:00 +0200

    retake TIP push 2

--- dash_application/navigation.py
+++ dash_application/navigation.py
@@ -2,13 +2,13 @@ import dash_bootstrap_components as dbc
 
 navbar = dbc.NavbarSimple(
     children=[
-        dbc.NavItem(dbc.NavLink("Logout", href='/logout')),
+        dbc.NavItem(dbc.NavLink("Logout", href='http://127.0.0.1:5000/logout')),
         dbc.DropdownMenu(
             children=[
                 dbc.DropdownMenuItem("List of KPIs", header=True),
-                dbc.DropdownMenuItem("KPI 1", href="/kpi1/"),
-                dbc.DropdownMenuItem("KPI 2", href="/kpi2/"),
-                dbc.DropdownMenuItem("KPI 3", href="/kpi3/"),
+                dbc.DropdownMenuItem("KPI 1", href="http://127.0.0.1:5000/kpi1/"),
+                dbc.DropdownMenuItem("KPI 2", href="http://127.0.0.1:5000/kpi2/"),
+                dbc.DropdownMenuItem("KPI 3", href="http://127.0.0.1:5000/kpi3/"),
             ],
             nav=True,
             in_navbar=True,
@@ -16,7 +16,7 @@ navbar = dbc.NavbarSimple(
         ),
     ],
     brand="IBERIA",
-    brand_href="/",
+    brand_href="http://127.0.0.1:5000/",
     color="primary",
     dark=True,
     fluid=True,
